85bb83f30e4a409fd4b1ab4af39b408838047beb
suryo <deaglefrenzy@gmail.com>
2025-08-13 10:55:15 +0800

    update

--- padel/input.php
+++ padel/input.php
@@ -3,13 +3,17 @@
         New Match
         <div>
             <?php
-            $q = mysqli_query($conn, "SELECT * FROM padelist ORDER BY team, name ASC");
+            if ($page == "men") $jk = "L";
+            else $jk = "P";
+            $query = "SELECT * FROM padelist WHERE jk='$jk' ORDER BY team, name ASC";
+            $q = mysqli_query($conn, $query) or die(mysqli_error($conn));
             $players = [];
             while ($gg = mysqli_fetch_array($q)) {
                 $players[] = $gg;
             }
             ?>
             <select name="pa1" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
+                <option value="">Pilih player 1 kiri</option>
                 <?php foreach ($players as $p) { ?>
                     <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
                         <?= $p['name'] . " (" . $p['team'] . ")" ?>
@@ -18,6 +22,7 @@
             </select>
             <br>
             <select name="pa2" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
+                <option value="">Pilih player 2 kiri</option>
                 <?php foreach ($players as $p) { ?>
                     <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
                         <?= $p['name'] . " (" . $p['team'] . ")" ?>
@@ -28,6 +33,7 @@
             vs
             <br>
             <select name="pb2" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
+                <option value="">Pilih player 1 kanan</option>
                 <?php foreach ($players as $p) { ?>
                     <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
                         <?= $p['name'] . " (" . $p['team'] . ")" ?>
@@ -36,12 +42,14 @@
             </select>
             <br>
             <select name="pb2" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
+                <option value="">Pilih player 2 kanan</option>
                 <?php foreach ($players as $p) { ?>
                     <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
                         <?= $p['name'] . " (" . $p['team'] . ")" ?>
                     </option>
                 <?php } ?>
             </select>
+            <br><br>
             <button type="submit" name="action" value="tambah match" <?php echo $dis; ?> class="w3-button w3-green" style="margin-bottom: 10px;">
                 <i class="fa-solid fa-plus-circle"></i> Tambah Match
             </button>
