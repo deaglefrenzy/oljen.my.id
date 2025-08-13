7e9a63ed2e78b53e14aabfbbe701f9b6d8eb58f6
suryo <deaglefrenzy@gmail.com>
2025-08-13 10:51:24 +0800

    update

--- padel/input.php
+++ padel/input.php
@@ -9,7 +9,33 @@
                 $players[] = $gg;
             }
             ?>
-            <select name="player">
+            <select name="pa1" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
+                <?php foreach ($players as $p) { ?>
+                    <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
+                        <?= $p['name'] . " (" . $p['team'] . ")" ?>
+                    </option>
+                <?php } ?>
+            </select>
+            <br>
+            <select name="pa2" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
+                <?php foreach ($players as $p) { ?>
+                    <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
+                        <?= $p['name'] . " (" . $p['team'] . ")" ?>
+                    </option>
+                <?php } ?>
+            </select>
+            <br>
+            vs
+            <br>
+            <select name="pb2" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
+                <?php foreach ($players as $p) { ?>
+                    <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
+                        <?= $p['name'] . " (" . $p['team'] . ")" ?>
+                    </option>
+                <?php } ?>
+            </select>
+            <br>
+            <select name="pb2" class="w3-select w3-border w3-round" style="width: 48%; display: inline-block; margin-right: 4px;">
                 <?php foreach ($players as $p) { ?>
                     <option value="<?= $p['id'] ?>" <?php if ($p['id'] == $pa1) echo "selected"; ?>>
                         <?= $p['name'] . " (" . $p['team'] . ")" ?>
