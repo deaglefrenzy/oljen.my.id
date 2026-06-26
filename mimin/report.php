e5e0587293edc52de92b266e85ae451ea7698847
suryo <deaglefrenzy@gmail.com>
2026-06-26 13:42:28 +0800

    report fix

--- mimin/report.php
+++ mimin/report.php
@@ -8,10 +8,8 @@ $categoryColors = [
     'w3-sand',
     'w3-khaki'
 ];
-
 $categoryClass = [];
 $i = 0;
-
 $query = "
     SELECT
         o.*,
@@ -28,7 +26,6 @@ $query = "
 ";
 $result = mysqli_query($conn, $query);
 $size_order = [
-    '3XS',
     '2XS',
     'XS',
     'S',
@@ -40,37 +37,25 @@ $size_order = [
 ];
 $report = [];
 $memberReport = [];
-
 while ($row = mysqli_fetch_assoc($result)) {
-
     $category = $row['category'];
     $variant = $row['variant'];
     $size = $row['size'];
     $material = $row['material'];
     $member = $row['member_name'];
-
     $variantShort = str_replace(
         ['Lengan Pendek', 'Lengan Panjang'],
         ['Pendek', 'Panjang'],
         $variant
     );
-
     // Existing report
-    $report[$category][$variantShort][$size][] = $member;
-
+    $report[$material][$category][$variantShort][$size][] = $member;
     // Member report
     // $memberReport[$member][] = [
     //     'category' => $category,
     //     'text' => "{$category} {$variantShort} {$material} {$size}"
     // ];
 }
-
-foreach ($report as $category => $variants) {
-    $categoryClass[$category] =
-        $categoryColors[$i % count($categoryColors)];
-    $i++;
-}
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -115,108 +100,120 @@ foreach ($report as $category => $variants) {
             <header class="w3-container w3-light-grey">
                 <h3>Summary</h3>
             </header>
-            <table class="w3-table w3-bordered w3-small">
-                <tr class="w3-light-grey">
-                    <th>Category</th>
-                    <?php foreach ($size_order as $size): ?>
-                        <th class="w3-center"><?= $size ?></th>
-                    <?php endforeach; ?>
-                    <th class="w3-right">Total</th>
-                </tr>
-
-                <?php
-                $grandTotal = 0;
-                ?>
-
-                <?php foreach ($report as $category => $variants): ?>
-                    <?php foreach ($variants as $variant => $sizes): ?>
-                        <?php
-                        $total = 0;
-                        ?>
-                        <tr>
-                            <td class="<?= $categoryClass[$category] ?>"><?= $category . " " . $variant ?></td>
-
+            <?php
+            $grandTotal = 0;
+            $grandSizeTotals = array_fill_keys($size_order, 0);
+            ?>
+            <?php foreach ($report as $material => $categories): ?>
+                <div class="w3-margin-bottom">
+                    <h4 class="w3-blue-grey w3-padding">
+                        <?= htmlspecialchars($material) ?>
+                    </h4>
+                    <table class="w3-table w3-bordered w3-small">
+                        <tr class="w3-light-grey">
+                            <th>Category</th>
                             <?php foreach ($size_order as $size): ?>
-                                <?php
-                                $qty = count($sizes[$size] ?? []);
-                                $total += $qty;
-                                ?>
+                                <th class="w3-center w3-border"><?= $size ?></th>
+                            <?php endforeach; ?>
+                            <th class="w3-right">Total</th>
+                        </tr>
+                        <?php
+                        $materialTotal = 0;
+                        $materialSizeTotals = array_fill_keys($size_order, 0);
+                        ?>
+                        <?php foreach ($categories as $category => $variants): ?>
+                            <?php foreach ($variants as $variant => $sizes): ?>
+                                <?php $rowTotal = 0; ?>
+                                <tr>
+                                    <td class="<?= $categoryClass[$category] ?>">
+                                        <?= htmlspecialchars($category . ' ' . $variant) ?>
+                                    </td>
+                                    <?php foreach ($size_order as $size): ?>
+                                        <?php
+                                        $qty = count($sizes[$size] ?? []);
+                                        $rowTotal += $qty;
+                                        $materialTotal += $qty;
+                                        $materialSizeTotals[$size] += $qty;
+                                        ?>
+                                        <td class="w3-center w3-border">
+                                            <?= $qty ?: '' ?>
+                                        </td>
+                                    <?php endforeach; ?>
+                                    <td class="w3-right">
+                                        <b><?= $rowTotal ?></b>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php endforeach; ?>
+                        <!-- MATERIAL SIZE TOTALS -->
+                        <tr class="w3-pale-yellow">
+                            <td><b>Size Totals</b></td>
+                            <?php foreach ($size_order as $size): ?>
                                 <td class="w3-center w3-border">
-                                    <?= $qty ?: '' ?>
+                                    <b><?= $materialSizeTotals[$size] ?: '' ?></b>
                                 </td>
                             <?php endforeach; ?>
-
-                            <td class="w3-right"><b><?= $total ?></b></td>
+                            <td class="w3-right">
+                                <b><?= $materialTotal ?></b>
+                            </td>
                         </tr>
-
-                        <?php
-                        $grandTotal += $total;
-                        ?>
-                    <?php endforeach; ?>
-                <?php endforeach; ?>
-
-                <!-- GRAND TOTAL ROW -->
-                <tr class="w3-light-grey">
-                    <td colspan="<?= 1 + count($size_order) ?>">
-                        <b>Grand Total</b>
-                    </td>
-                    <td class="w3-right"><b><?= $grandTotal ?></b></td>
-                </tr>
-            </table>
+                    </table>
+                </div>
+            <?php endforeach; ?>
         </div>
         <div class="w3-margin-top"></div>
         <div class="w3-card w3-white">
             <header class="w3-container w3-light-grey">
                 <h3>Details</h3>
             </header>
-            <table class="w3-table w3-bordered w3-small w3-white">
-                <tr class="w3-light-grey">
-                    <th>Category</th>
-                    <th>Size</th>
-                    <th>Qty</th>
-                    <th>Members</th>
-                </tr>
-                <?php foreach ($report as $category => $variants): ?>
-                    <?php foreach ($variants as $variant => $sizes): ?>
-                        <?php foreach ($sizes as $size => $members):
-                            ?>
-                            <tr>
-                                <td class="<?= $categoryClass[$category] ?>" style="white-space: nowrap;">
-                                    <?= $category . " " . $variant ?>
-                                </td>
-                                <td><?= $size ?></td>
-                                <td><?= count($members) ?></td>
-                                <td>
-                                    <?php foreach ($members as $member): ?>
-                                        <span class="w3-border w3-round w3-padding-small" style="display:inline-block;margin:2px;">
-                                            <?= htmlspecialchars($member) ?>
-                                        </span>
-                                    <?php endforeach; ?>
-                                </td>
-                            </tr>
+            <?php foreach ($report as $material => $categories): ?>
+                <table class="w3-table w3-bordered w3-small w3-white">
+                    <!-- MATERIAL HEADER -->
+                    <tr class="w3-blue-grey">
+                        <td colspan="4" style="font-size:14px;">
+                            <b><?= htmlspecialchars($material) ?></b>
+                        </td>
+                    </tr>
+                    <tr class="w3-light-grey">
+                        <th>Category</th>
+                        <th>Size</th>
+                        <th>Qty</th>
+                        <th>Members</th>
+                    </tr>
+                    <?php foreach ($categories as $category => $variants): ?>
+                        <?php foreach ($variants as $variant => $sizes): ?>
+                            <?php foreach ($sizes as $size => $members): ?>
+                                <tr>
+                                    <td class="<?= $categoryClass[$category] ?>" style="white-space: nowrap;">
+                                        <?= htmlspecialchars($category . ' ' . $variant) ?>
+                                    </td>
+                                    <td><?= htmlspecialchars($size) ?></td>
+                                    <td><?= count($members) ?></td>
+                                    <td>
+                                        <?php foreach ($members as $member): ?>
+                                            <span class="w3-border w3-round w3-padding-small" style="display:inline-block;margin:2px;">
+                                                <?= htmlspecialchars($member) ?>
+                                            </span>
+                                        <?php endforeach; ?>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
                         <?php endforeach; ?>
                     <?php endforeach; ?>
-                <?php endforeach; ?>
-            </table>
+                </table>
+            <?php endforeach; ?>
         </div>
-
         <?php if (count($memberReport) > 0): ?>
             <div class="page-break"></div>
-
             <div class="w3-card w3-white w3-margin-top">
                 <header class="w3-container w3-light-grey w3-margin-bottom">
                     <h3>Orders By Member</h3>
                 </header>
-
                 <div class="w3-row-padding" style="font-size:11px;">
-
                     <?php ksort($memberReport); ?>
-
                     <?php foreach ($memberReport as $member => $items): ?>
                         <div class="w3-third w3-margin-bottom">
-
                             <div class="w3-border w3-round" style="padding:6px;">
-
                                 <div style="
                         font-weight:bold;
                         font-size:12px;
@@ -229,7 +226,6 @@ foreach ($report as $category => $variants) {
                     ">
                                     <?= htmlspecialchars($member) ?>
                                 </div>
-
                                 <div style="
                         display:flex;
                         flex-wrap:wrap;
@@ -249,12 +245,9 @@ foreach ($report as $category => $variants) {
                                         </span>
                                     <?php endforeach; ?>
                                 </div>
-
                             </div>
-
                         </div>
                     <?php endforeach; ?>
-
                 </div>
             </div>
         <?php endif; ?>
